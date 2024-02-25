2a1a42bf40c9c84f577ebea0a1a2e4f4926690c2
The-British <161207929+The-British@users.noreply.github.com>
2024-02-25 10:47:02 +0000

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["📜 WATCHING OVER Springfield"];
 
 
 let currentIndex = 0;
